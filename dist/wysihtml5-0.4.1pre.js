21e4f07778ca67a80ed62bb9a476cae020526c21
mralexlau <laux0083@gmail.com>
2015-02-23 11:19:17 -0600

    Fixes bug in chrome where the cursor jumps after pressing enter

--- dist/wysihtml5-0.4.1pre.js
+++ dist/wysihtml5-0.4.1pre.js
@@ -8602,10 +8602,10 @@ wysihtml5.views.View = Base.extend(
       }, 0);
     });
 
-    // --------- neword event ---------
+    // --------- newword event ---------
     dom.observe(element, "keyup", function(event) {
       var keyCode = event.keyCode;
-      if (keyCode === wysihtml5.SPACE_KEY || keyCode === wysihtml5.ENTER_KEY) {
+      if (keyCode === wysihtml5.SPACE_KEY || (!isChrome && keyCode === wysihtml5.ENTER_KEY)) {
         that.parent.fire("newword:composer");
       }
     });
